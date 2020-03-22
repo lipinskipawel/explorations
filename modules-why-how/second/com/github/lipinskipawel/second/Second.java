3a2366f469044fcc5154b026a252dacd1e5de4cc
Pawel Lipinski <plipinski@atlassian.com>
2020-03-22 20:43:41 +0100

    Remove output/mlib/first.jar
    Run from command line both commands.
     - java -classpath ....
     - java -p .....
    
    The module will not even start to work due to module graph.

--- modules-why-how/second/com/github/lipinskipawel/second/Second.java
+++ modules-why-how/second/com/github/lipinskipawel/second/Second.java
@@ -8,6 +8,7 @@ public class Second {
 
 
 	public static void main(String[] args) {
+		System.out.println("Code is running");
 		System.out.println(new Second().getInfo());
 		System.out.println(new First().getInfo());
 	}
