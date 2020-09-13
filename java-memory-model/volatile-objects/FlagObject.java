ea4e02bab6490dcc11c43dea10ba6d2e904d04bf
Pawel Lipinski <plipinski@atlassian.com>
2020-09-13 11:51:57 +0200

    JMM: Volatile objects

---
+++ java-memory-model/volatile-objects/FlagObject.java
@@ -0,0 +1,13 @@
+
+final class FlagObject {
+
+    private final boolean flag;
+
+    FlagObject(final boolean flag) {
+        this.flag = flag;
+    }
+
+    boolean isNotDone() {
+        return !flag;
+    }
+}
---
+++ java-memory-model/volatile-objects/MainVolatileObject.java
@@ -0,0 +1,25 @@
+
+final class MainVolatileObject {
+
+    private static volatile FlagObject flag = new FlagObject(false);
+
+    public static void main(String[] args) throws InterruptedException {
+
+        final Thread background = new Thread(() -> {
+            System.out.println("Background thread has been started.");
+            while (flag.isNotDone()) {
+                someImportantWork();
+            }
+            System.out.println("Background thread has finishes job.");
+        });
+
+        background.start();
+        Thread.sleep(1000);
+        flag = new FlagObject(true);
+    }
+
+    private static void someImportantWork() {
+        int some = 4;
+        // System.out.println("Working");
+    }
+}
