6b68b5c715d5f053c4347750568677612ee7e8e0
Pawel Lipinski <lipinskijob@gmail.com>
2024-08-14 15:23:26 +0200

    Use Kotlin DSL for Gradle

---
+++ java-code/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    `java-library`
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+testing {
+    suites {
+        val test by getting(JvmTestSuite::class) {
+            useJUnitJupiter()
+        }
+    }
+}
