b72fb5ed217d8f26f1bc6b9cc1fc2384c187214d
Pawel Lipinski <lipinskijob@gmail.com>
2025-03-06 13:23:38 +0100

    Solve 3 leetcode java

---
+++ java-code/src/main/java/com/github/lipinskipawel/leetcode/LongestSubString.java
@@ -0,0 +1,31 @@
+package com.github.lipinskipawel.leetcode;
+
+import java.util.HashSet;
+
+// 3
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+final class LongestSubString {
+
+    public int lengthOfLongestSubstring(String s) {
+        var longest = 0;
+        var left = 0;
+
+        final var characters = new HashSet<Character>();
+
+        for (var i = 0; i < s.length(); i++) {
+            if (!characters.contains(s.charAt(i))) {
+                characters.add(s.charAt(i));
+                longest = Math.max(longest, characters.size());
+            } else {
+                longest = Math.max(longest, characters.size());
+                while (characters.contains(s.charAt(i))) {
+                    characters.remove(s.charAt(left));
+                    left += 1;
+                }
+                characters.add(s.charAt(i));
+            }
+        }
+
+        return longest;
+    }
+}
---
+++ java-code/src/test/java/com/github/lipinskipawel/leetcode/LongestSubStringTest.java
@@ -0,0 +1,33 @@
+package com.github.lipinskipawel.leetcode;
+
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.of;
+
+class LongestSubStringTest implements WithAssertions {
+
+    private final LongestSubString longestSubString = new LongestSubString();
+
+    private static Stream<Arguments> strings() {
+        return Stream.of(
+            of("abcabcbb", 3),
+            of("bbbbb", 1),
+            of("pwwkew", 3),
+            of(" ", 1),
+            of("dvdf", 3)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("strings")
+    void subString(String input, int expected) {
+        final var result = longestSubString.lengthOfLongestSubstring(input);
+
+        assertThat(result).isEqualTo(expected);
+    }
+}
